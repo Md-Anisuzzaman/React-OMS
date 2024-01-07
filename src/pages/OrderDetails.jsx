b34afec70e84191ddfe397a8a11ff9c9bf74dd85
Anisuzzaman <anisuzzaman1199@gmail.com>
2024-01-07 23:46:40 +0600

    Selected product data showing

---
+++ src/pages/OrderDetails.jsx
@@ -0,0 +1,107 @@
+import { Button} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { BASE_URL, PASSWORD, USERNAME, blackColor } from "./../../varible";
+import base64 from "base-64";
+import ProductTable from "../Component/ProductTable";
+import { Link } from "react-router-dom";
+
+import SeletedProduct from './../Component/SeletedProduct';
+
+
+
+const OrderDetails = () => {
+
+
+  const [showProductData, setShowProductData] = useState(true);
+  const [showOrderData, setShowOrderData] = useState(false);
+  const [productData, setProductData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // console.log({ productData });
+
+
+  const handleProductButtonPress = () => {
+    setShowProductData(true);
+    setShowOrderData(false);
+  };
+
+  const handleOrderButtonPress = () => {
+    setShowProductData(false);
+    setShowOrderData(true);
+  };
+
+  const fetchProductDatax = async () => {
+    try {
+      const authHeader = "Basic " + base64.encode(USERNAME + ":" + PASSWORD);
+      const response = await fetch(`${BASE_URL}/api/ProductApi/GetAllProduct`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: authHeader,
+        },
+      });
+      const result = await response.json();
+      // console.log("jsonData",typeof(result));
+      console.log({ result });
+      setProductData(result)
+      setIsLoading(false);
+      // return jsonData;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+
+    }
+  }
+
+  useEffect(() => {
+    fetchProductDatax();
+  }, []);
+
+
+
+  return (
+    <div>
+      <h3 style={{ textAlign: "center" }}>Order Details</h3>
+
+      <div
+        style={{
+          display: "flex",
+          gap: 5,
+          alignItems: "center",
+          justifyContent: "center",
+          marginBottom: "50px",
+        }}
+      >
+        <Button variant="contained" onClick={handleProductButtonPress}
+        >Product List</Button>
+        <Link to="/selectedproduct"><Button variant="contained"
+          onClick={handleOrderButtonPress}
+        >Order Details</Button></Link>
+      </div>
+
+      {/* product data part  */}
+      <>
+        {
+          showProductData && (
+            <ProductTable Products={productData} />
+          )
+        }
+
+      </>
+
+
+
+      <>
+        {/* <SeletedProduct /> */}
+      </>
+
+
+    </div>
+  );
+};
+
+export default OrderDetails;
+
+//  first  need two button
+//fetch product data with a qty box
+//those selected qty box was show in orderDetails
+//orderDetails have submit button
